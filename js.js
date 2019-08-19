0f3f926d64eafa8a0575c532bcbfb10c9658e6d9
dennis <denni.k96@gmx.de>
2019-08-19 16:39:28 +0200

    minor fix

--- js.js
+++ js.js
@@ -14,17 +14,16 @@ addItem = function () {
     let element = document.createElement("li");
     element.id = "element" + counter;
     element.innerHTML = "<p class='hoverable'>X</p>" + "<p>" + name + "</p>" + "<p class='amount'>" + amount + " Stück</p>" + "<p class='value'>" + value + "€</p>";
-    element.onclick = function() {
+    element.getElementsByClassName('hoverable')[0].onclick = function() {
 
-       let value = this.getElementsByClassName('value')[0].innerHTML;
-       let amount = this.getElementsByClassName('amount')[0].innerHTML;
+       let value = this.parentNode.getElementsByClassName('value')[0].innerHTML;
+       let amount = this.parentNode.getElementsByClassName('amount')[0].innerHTML;
 
-
-
-      //  console.log(value.substring(0, value.length-1));
+       //  console.log(value.substring(0, value.length-1));
       //  console.log(amount.substring(0, amount.length-6));
+        this.parentNode.parentNode.removeChild(this.parentNode);
 
-        this.parentNode.removeChild(this);
+       // this.parentNode.removeChild(this);
 
         total = total - parseInt(value.substr(0, value.length-1)) * parseInt(amount.substr(0, amount.length-6));
         showTotalValue();
